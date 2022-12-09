8fe658056835b76d45a264bb67163c4209bb4cc1
chiefofstack <chiefofstack@gmail.com>
2022-12-09 02:26:43 +0000

    improve the logic of the sticky nav, usee inline styles instead of classes for the sliding animation

--- js/navigation.js
+++ js/navigation.js
@@ -1,31 +1,52 @@
 //Sticky head navigation
-
-const body = $("body");
+const header = $(".header");
 let lastScroll = 0;
+
 
 // When scrolled
 window.addEventListener("scroll", () => {
-
-  const currentScroll = window.pageYOffset;
+    const currentScroll = window.pageYOffset;
     
-//   // If the scroll is at the top of the screen, ignore the rest of the function
-//   if (currentScroll <= 0) {
-//     body.removeClass("scroll-up");
-//   }
+    //if scrolled beyond the nav, slide the nav up or down
+    if(currentScroll > header.height()){
+        // if scrolled down, slide the nav up 
+        if (currentScroll > lastScroll) {
+            //  if have scrolled beyond the nav before, just do as normal slide up
+            if(header.hasClass('haveScrolledUp')){                
+                header.css({ "top": -header.height() });  
+                header.addClass("sticky");  
+            }
+            else{ //if not, apply sticky position first and move up without transition
+                header.addClass("sticky"); 
+                header.css({ "top": -header.height(), "transition":"none" });
+            }
+        }
+        // if scrolled up, slide the nav down up
+        if (currentScroll < lastScroll) {            
+            header.removeAttr("style"); //clear styles to activate default transition
+            header.css({ "top":"0" });
+            header.addClass('haveScrolledUp');
+            
+        }
+            
+    }
 
-  // if scrolled down
-  if (currentScroll > lastScroll) {
-    body.removeClass("scroll-up");
-    body.addClass("scroll-down");
-  }
+    //if scrolled within the nav
+    else if (currentScroll <= header.height() && currentScroll != 0){   
+        // if scrolled up, return the nav default position
+        if (currentScroll < lastScroll) {
+            header.css({ "top":"0" });       
+        } 
+    }    
 
-  // if scrolled up
-  if (currentScroll < lastScroll) {
-    body.removeClass("scroll-down");
-    body.addClass("scroll-up");
-  }
-
-  // Returns the current scroll Y offset and records it, to be checked against
-  // the next scroll
-  lastScroll = currentScroll;
+    // if scrolled to the top
+    else if(currentScroll == 0){
+        header.removeAttr("style");
+        header.removeClass('sticky');
+        header.removeClass('haveScrolledUp'); 
+        
+    }
+    
+    // store next scroll for next time
+    lastScroll = currentScroll;
 });
