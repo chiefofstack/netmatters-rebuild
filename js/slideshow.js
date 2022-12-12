ee2a0a26c023ba2a02ba0133f0b459cc5ebbefa0
chiefofstack <chiefofstack@gmail.com>
2022-12-12 16:09:29 +0000

    add  banner slideshow settings to a separate file

---
+++ js/slideshow.js
@@ -0,0 +1,28 @@
+/* -------------- Slideshow -------------- */
+//Banner Slideshow
+var slider = tns({
+    container: '.banner.slider',
+    mouseDrag: true,
+    touch: true,
+    controls: false,
+    autoplay: true,
+    lazyload: true,
+    navPosition: "bottom",
+    nav: true,
+    autoplayHoverPause: true,
+    autoplayButtonOutput: false
+});
+
+//Partners Slideshow
+// var partners = tns({
+//     container: ".partner.slider",
+//     slideBy: "page",
+//     loop: true,
+//     touch: true,
+//     controls: false,
+//     mouseDrag: true,
+//     swipeAngle: false,
+//     autoplay: true,
+//     speed: 400,
+//     fixedWidth: 250,
+// });
