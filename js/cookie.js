70e0e95c7d8fd3e79818d9780dc4d4f6dcd2a304
chiefofstack <chiefofstack@gmail.com>
2022-12-13 18:11:09 +0000

    made functions to set and get cookie values, used it to store the prefence on local storage + added click events on the local to hide/unhide modal and consent overlay

---
+++ js/cookie.js
@@ -0,0 +1,81 @@
+/* -------------- Cookie -------------- */
+
+function setCookie(cookieName, cookieValue, expirationDays) {
+    const date = new Date();
+    date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+date.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+  
+function getCookie(cookieName) {
+    let name = cookieName + "=";
+    let cookieAttributes = document.cookie.split(';');
+    for(let i = 0; i < cookieAttributes.length; i++) {
+        let cookieValue = cookieAttributes[i];
+        while (cookieValue.charAt(0) == ' ') {
+            cookieValue = cookieValue.substring(1);
+        }
+        if (cookieValue.indexOf(name) == 0) {
+            return cookieValue.substring(name.length, cookieValue.length);
+        }
+    }
+    return "";
+}
+
+// show cookie dialog
+$(window).on('load', () => {   
+    if(!getCookie("accepted")){
+        overlay.addClass("active");    
+        cookieConsent.addClass("active");         
+        body.css({"overflow-y":"hidden"});
+    }        
+});
+
+btnChange.on('click', () => {
+    //hide consent modal
+    overlay.removeClass("active");
+    cookieConsent.removeClass("active");
+
+    //show cookie overlay
+    cookieOverlay.addClass("active");
+    body.css({"overflow-y":"hidden"});
+    cookieOverlay.css({"overflow-y":"scroll"});
+});
+
+btnAccept.on('click', () => {    
+    setCookie("accepted", true, 365);
+    overlay.removeClass("active"); 
+    cookieConsent.removeClass("active");    
+    body.css({"overflow-y":"scroll"});
+});
+
+
+/* -------------- Cookie Overlay -------------- */
+
+btnCancel.on('click', () => {    
+    //show consent modal again
+    overlay.addClass("active");    
+    cookieConsent.addClass("active"); 
+
+    //hide consent Overlay
+    cookieOverlay.removeClass("active");
+    cookieOverlay.css({"overflow-y":"scroll"});
+
+});
+
+btnContinue.on('click', () => {    
+    setCookie("accepted", true, 365);
+    cookieOverlay.removeClass("active");     
+    body.css({"overflow-y":"scroll"});
+});
+
+btnPreferences.on('click', () => {
+    if(btnPreferences.hasClass('active')){
+        btnPreferences.removeClass("active");
+        prefTable.removeAttr("style");
+    }
+    else{
+        btnPreferences.addClass("active");
+        prefTable.css({ "display": "block"});
+    }
+});
