0af8a3f524a8712fb12c78982b3bc5cb300c92a0
chiefofstack <chiefofstack@gmail.com>
2023-02-08 09:53:11 +0000

    move overlay element to its own file

---
+++ includes/overlay.php
@@ -0,0 +1,123 @@
+<?php
+        <!-- Overlay -->
+        <div class="overlay">
+            <div class="cookie-consent">
+                <div class="mdl-header">
+                    <h4>Cookies Consent</h4>
+                </div>
+                <div class="mdl-body">
+                    <p>Our website uses cookies. This helps us provide you with a good experience on our website. To see what cookies we use and what they do, and to opt-in on non-essential cookies click "change settings". For a detailed explanation, click on "Privacy Policy" otherwise click "Accept Cookies" to enter.</p>
+                </div>
+                <div class="mdl-footer">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <a href="#" class="btn btn-default btn-change">Change Settings</a>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <a href="#" class="btn btn-design btn-accept">Accept Cookies</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Cookie Overlay -->
+        <div class="cookie-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h4>Cookies Preferences</h4>
+                        <p>Netmatters uses cookies on their website. Cookies are small text files that are stored on your computer or other device by websites that you visit. This page explains the cookies we use and what we use them for, and lets you turn them on or off. (Some cookies are necessary in order for our website to work properly.) We also explain below which other companies use cookies on our website and what they use them for, and lets you turn those other companies' cookies on or off.</p>
+                        <p>Our website uses cookies in order to make the website easier to use, to support the provision of information and functionality to you, as well as to provide us with information about how the website is used so that we can make sure it is as up to date, relevant and error free as we can. We also use cookies to try to ensure that our online adverts reflect the interests of web users. Further information about the types of cookies that are used on our website is set out box below.</p>
+                        <p>As well as the options provided below, you can choose to restrict or block cookies through your browser settings at any time. For more information about how to do this, and about cookies in general, you can visit www.cookiepedia.co.uk and www.youronlinechoices.eu. However, please be aware that restricting or blocking cookies set on our website may impact the functionality or performance of the website, or prevent you from using certain services provided through the website</p>
+                        <p>Please note that third parties (including, for example, advertising networks and providers of external services like website analysis services) may also use cookies, over which we have no control, although we may receive services from these third parties (including, for example, for targeted advertising purposes and website analytics). These cookies are likely to be performance cookies or targeting cookies (as described below).</p>
+
+                        <h2>Functional Cookies</h2>
+                        <p>Functional cookies allow our website to remember choices you make, such as your user name, log in details or language preferences, and any customisations you make to pages on our website during your visit.</p>
+                        <p class="example">Examples of how we use these cookies include:</p>
+                        <ul>
+                            <li>Live Chat</li>
+                        </ul>
+
+                        <div class="option-group">
+                            <button class="btn btn-design">Disable</button>
+                            <button class="btn btn-disabled">Enable</button>
+                        </div>
+
+                        <h2>Performance & Analytics</h2>
+                        <p>These cookies help us understand how people use our website. They collect information such as which pages on our website visitors go to most often, which features they use, and which websites people have visited before they visit ours. We use this information to improve our website and provide a better user experience.</p>
+                        <p class="example">Examples of how we use these cookies include:</p>
+                        <ul>
+                            <li>Monitoring and providing statistics on how our website is used.</li>
+                            <li>Helping us improve our website by measuring any errors that occur.</li>
+                            <li>Testing the website's design and operability</li>
+                        </ul>
+                        <div class="option-group">
+                            <button class="btn btn-design">Disable</button>
+                            <button class="btn btn-disabled">Enable</button>
+                        </div>
+
+                        <p class="no-header">Different web browsers may use different methods for managing cookies. Please follow the instructions below, from the web browser manufacturers directly, to configure your browser settings*.</p>
+                        <ul>
+                            <li>Microsoft Internet Explorer (IE)</li>
+                            <li>Google Chrome</li>
+                            <li>Safari</li>
+                            <li>Firefox</li>
+                        </ul>
+
+                        <button class="btn btn-default btn-preferences">Show Detailed Preferences</button>
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <td colspan="50">Company</td>
+                                    <td colspan="50">Domain</td>
+                                    <td></td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td colspan="50">Hotjar Ltd</td>
+                                    <td colspan="50">hotjar.com</td>
+                                    <td> 
+                                        <button class="btn btn-design">Disable</button>
+                                        <button class="btn btn-disabled">Enable</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="50">Google Inc</td>
+                                    <td colspan="50">google.com, googletagmanager.com, google-analytics.com</td>
+                                    <td> 
+                                        <button class="btn btn-design">Disable</button>
+                                        <button class="btn btn-disabled">Enable</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="50">Hotjar Ltd</td>
+                                    <td colspan="50">hotjar.com</td>
+                                    <td> 
+                                        <button class="btn btn-design">Disable</button>
+                                        <button class="btn btn-disabled">Enable</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="50">Hotjar Ltd</td>
+                                    <td colspan="50">hotjar.com</td>
+                                    <td> 
+                                        <button class="btn btn-design">Disable</button>
+                                        <button class="btn btn-disabled">Enable</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <div class="option-group foot">
+                            <button class="btn btn-default btn-cancel">Cancel</button>
+                            <button class="btn btn-design btn-continue">Continue to Website</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+?>
