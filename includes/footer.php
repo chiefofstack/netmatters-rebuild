63df5714219bff52f79d0cc527c2a6ef671313fd
chiefofstack <chiefofstack@gmail.com>
2023-02-08 09:49:37 +0000

    separate the footer for faster maintenance

---
+++ includes/footer.php
@@ -0,0 +1,301 @@
+        </main>
+        <!-- Overlay -->
+        <div class="overlay">
+            <div class="cookie-consent">
+                <div class="mdl-header">
+                    <h4>Cookies Consent</h4>
+                </div>
+                <div class="mdl-body">
+                    <p>Our website uses cookies. This helps us provide you with a good experience on our website. To see what cookies we use and what they do, and to opt-in on non-essential cookies click "change settings". For a detailed explanation, click on "Privacy Policy" otherwise click "Accept Cookies" to enter.</p>
+                </div>
+                <div class="mdl-footer">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <a href="#" class="btn btn-default btn-change">Change Settings</a>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <a href="#" class="btn btn-design btn-accept">Accept Cookies</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Cookie Overlay -->
+        <div class="cookie-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h4>Cookies Preferences</h4>
+                        <p>Netmatters uses cookies on their website. Cookies are small text files that are stored on your computer or other device by websites that you visit. This page explains the cookies we use and what we use them for, and lets you turn them on or off. (Some cookies are necessary in order for our website to work properly.) We also explain below which other companies use cookies on our website and what they use them for, and lets you turn those other companies' cookies on or off.</p>
+                        <p>Our website uses cookies in order to make the website easier to use, to support the provision of information and functionality to you, as well as to provide us with information about how the website is used so that we can make sure it is as up to date, relevant and error free as we can. We also use cookies to try to ensure that our online adverts reflect the interests of web users. Further information about the types of cookies that are used on our website is set out box below.</p>
+                        <p>As well as the options provided below, you can choose to restrict or block cookies through your browser settings at any time. For more information about how to do this, and about cookies in general, you can visit www.cookiepedia.co.uk and www.youronlinechoices.eu. However, please be aware that restricting or blocking cookies set on our website may impact the functionality or performance of the website, or prevent you from using certain services provided through the website</p>
+                        <p>Please note that third parties (including, for example, advertising networks and providers of external services like website analysis services) may also use cookies, over which we have no control, although we may receive services from these third parties (including, for example, for targeted advertising purposes and website analytics). These cookies are likely to be performance cookies or targeting cookies (as described below).</p>
+
+                        <h2>Functional Cookies</h2>
+                        <p>Functional cookies allow our website to remember choices you make, such as your user name, log in details or language preferences, and any customisations you make to pages on our website during your visit.</p>
+                        <p class="example">Examples of how we use these cookies include:</p>
+                        <ul>
+                            <li>Live Chat</li>
+                        </ul>
+
+                        <div class="option-group">
+                            <button class="btn btn-design">Disable</button>
+                            <button class="btn btn-disabled">Enable</button>
+                        </div>
+
+                        <h2>Performance & Analytics</h2>
+                        <p>These cookies help us understand how people use our website. They collect information such as which pages on our website visitors go to most often, which features they use, and which websites people have visited before they visit ours. We use this information to improve our website and provide a better user experience.</p>
+                        <p class="example">Examples of how we use these cookies include:</p>
+                        <ul>
+                            <li>Monitoring and providing statistics on how our website is used.</li>
+                            <li>Helping us improve our website by measuring any errors that occur.</li>
+                            <li>Testing the website's design and operability</li>
+                        </ul>
+                        <div class="option-group">
+                            <button class="btn btn-design">Disable</button>
+                            <button class="btn btn-disabled">Enable</button>
+                        </div>
+
+                        <p class="no-header">Different web browsers may use different methods for managing cookies. Please follow the instructions below, from the web browser manufacturers directly, to configure your browser settings*.</p>
+                        <ul>
+                            <li>Microsoft Internet Explorer (IE)</li>
+                            <li>Google Chrome</li>
+                            <li>Safari</li>
+                            <li>Firefox</li>
+                        </ul>
+
+                        <button class="btn btn-default btn-preferences">Show Detailed Preferences</button>
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <td colspan="50">Company</td>
+                                    <td colspan="50">Domain</td>
+                                    <td></td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td colspan="50">Hotjar Ltd</td>
+                                    <td colspan="50">hotjar.com</td>
+                                    <td> 
+                                        <button class="btn btn-design">Disable</button>
+                                        <button class="btn btn-disabled">Enable</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="50">Google Inc</td>
+                                    <td colspan="50">google.com, googletagmanager.com, google-analytics.com</td>
+                                    <td> 
+                                        <button class="btn btn-design">Disable</button>
+                                        <button class="btn btn-disabled">Enable</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="50">Hotjar Ltd</td>
+                                    <td colspan="50">hotjar.com</td>
+                                    <td> 
+                                        <button class="btn btn-design">Disable</button>
+                                        <button class="btn btn-disabled">Enable</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="50">Hotjar Ltd</td>
+                                    <td colspan="50">hotjar.com</td>
+                                    <td> 
+                                        <button class="btn btn-design">Disable</button>
+                                        <button class="btn btn-disabled">Enable</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <div class="option-group foot">
+                            <button class="btn btn-default btn-cancel">Cancel</button>
+                            <button class="btn btn-design btn-continue">Continue to Website</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Sidebar -->
+        <div class="sidebar">           
+            <ul class="mobile-menu">
+                <li class="head">
+                    <a href="#">Contact Us</a>
+                </li>
+                <li class="head software">
+                    <a href="#">
+                        <i class="fas fa-th"></i><br>
+                        <small>Bespoke</small><br>Software
+                    </a>
+                </li>
+                <li class="first"><a href="#">Workflow</a></li>
+                <li><a href="#">Automation</a></li>
+                <li><a href="#">Integration</a></li>
+                <li><a href="#">Apps</a></li>
+                <li><a href="#">Database</a></li>
+                <li><a href="#">Sharepoint</a></li>
+                <li><a href="#">Management</a></li>
+                <li><a href="#">Business Central</a></li>
+                <li><a href="#">Internet of Things</a></li>
+                <li><a href="#">Intranet Development</a></li>
+                <li><a href="#">Customer Portal Development</a></li>
+                <li class="last"><a href="#">Reporting Hub</a></li>
+
+                <li class="head support">
+                    <a href="#">
+                        <i class="fa fa-tv"></i><br>
+                        <small>IT</small><br>Support
+                    </a>
+                </li>
+                <li class="first"><a href="#">Managed IT</a></li>
+                <li><a href="#">Business IT</a></li>
+                <li><a href="#">Office 365</a></li>
+                <li><a href="#">Consultancy</a></li>
+                <li><a href="#">Cloud Provider</a></li>
+                <li class="last"><a href="#">Data Backup</a></li>
+
+                <li class="head marketing">
+                    <a href="#">
+                        <i class="fas fa-signal" aria-hidden="true"></i><br>
+                        <small>Digital</small><br>Marketing
+                    </a>
+                </li>
+                <li class="first"><a href="#">Search (SEO)</a></li>
+                <li><a href="#">Paid (PPC)</a></li>
+                <li><a href="#">Conversion (CRO)</a></li>
+                <li><a href="#">Email</a></li>
+                <li><a href="#">Social Media</a></li>
+                <li class="last"><a href="#">Content</a></li>
+
+                <li class="head services">
+                    <a href="#">
+                        <i class="fas fa-phone-alt" aria-hidden="true"></i><br>
+                        <small>Telecoms</small><br>Services
+                    </a>
+                </li>
+                <li class="first"><a href="#">Business Mobile</a></li>
+                <li><a href="#">Hosted VOIP</a></li>
+                <li><a href="#">Business VOIP</a></li>
+                <li><a href="#">Business Broadband</a></li>
+                <li><a href="#">Lease Line</a></li>
+                <li class="last"><a href="#">3CX Systems</a></li>
+
+                <li class="head design">
+                    <a href="#">
+                        <i class="fas fa-code" aria-hidden="true"></i><br>
+                        <small>Web</small><br>Design
+                    </a>
+                </li>
+                <li class="first"><a href="#">Stylish Websites</a></li>
+                <li><a href="#">Ecommerce Stores</a></li>
+                <li><a href="#">Branding</a></li>
+                <li><a href="#">Apps</a></li>
+                <li><a href="#">Web Hosting</a></li>
+                <li class="last"><a href="#">Pay Monthly Websites</a></li>
+
+                <li class="head security">
+                    <a href="#">
+                        <i class="fas fa-shield-alt" aria-hidden="true"></i><br>
+                        <small>Cyber</small><br>Security
+                    </a>
+                </li>
+                <li class="first"><a href="#">Assessment</a></li>
+                <li><a href="#">Management</a></li>
+                <li><a href="#">Penetration Testing</a></li>
+                <li><a href="#">Cyber Essentials</a></li>
+                <li><a href="#">PCI/DSS</a></li>
+                <li class="last"><a href="#">Hacker Prevention</a></li>
+
+                <li class="head course">
+                    <a href="#">
+                        <i class="fas fa-graduation-cap" aria-hidden="true"></i><br><small>Developer</small>
+                        <br>Course
+                    </a>
+                </li>
+                <li class="first"><a href="#">Train for a Career in Tech</a></li>
+                <li><a href="#">Skills Bootcamp</a></li>
+                <li><a href="#">Scion Scheme Frequently Asked Questions</a></li>
+                <li class="last"><a href="#">Scion Collaborators</a></li>
+            </ul> 
+            <ul class="default-menu">
+                <li class="services-item">
+                    <a href="#">Services</a>
+                    <ul>
+                        <li><a href="#">Bespoke Software</a></li>
+                        <li><a href="#">IT Support</a></li>
+                        <li><a href="#">Digital Marketing</a></li>
+                        <li><a href="#">Telecoms Services</a></li>
+                        <li><a href="#">Web Design</a></li>
+                        <li><a href="#">Cyber Security</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Our Work</a>
+                    <ul>
+                        <li><a href="#">Case Studies</a></li>
+                    </ul> 
+                </li>
+                <li>
+                    <a href="#">Our Knowledge</a>
+                    <ul>
+                        <li><a href="#">Technologies</a></li>
+                        <li><a href="#">Industries</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Insights</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Training</a>
+                    <ul>
+                        <li><a href="#">Train for a Career in Tech</a></li>
+                        <li><a href="#">Skills Bootcamp</a></li>
+                        <li><a href="#">SCS Frequently Asked Questions</a></li>
+                        <li><a href="#">Scion Collaborators</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Events</a>
+                    <ul>
+                        <li><a href="#">Business Automation Seminar</a></li>
+                        <li><a href="#">GA4 Webinar</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Our Company</a>
+                    <ul>                        
+                        <li><a href="#">Our Careers</a></li>
+                        <li><a href="#">Our Team</a></li>                        
+                        <li><a href="#">Our Culture</a></li>
+                        <li><a href="#">Our Benefits</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Contact Us</a>
+                    <ul>
+                        <li><a href="#">Cambridge Office</a></li>
+                        <li><a href="#">Wymondham Office</a></li>
+                        <li><a href="#">Great Yarmouth Office</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Covid Risk Assessments</a>
+                </li>
+                <li class="support-item">
+                    <a href="#">Support</a>
+                </li>
+
+            </ul>             
+        </div>       
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>        
+        <script src="js/slideshow.js"></script>
+        <script src="js/navigation.js"></script>
+        <script src="js/sidebar.js"></script>
+        <script src="js/cookie.js"></script>
+    </body>
+</html>
