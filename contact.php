37071b2f7a7a722c34c1e8c06576ac507c4684cc
chiefofstack <chiefofstack@gmail.com>
2023-02-09 19:01:08 +0000

    offices section

---
+++ contact.php
@@ -0,0 +1,257 @@
+<?php
+$currentPage = "Contact";
+include 'includes/header.php';
+?>
+            <!-- Offices -->
+            <div class="offices">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="heading">
+                                <h3 class="section-title">Our Offices</h3>
+                            </div>                        
+                        </div>                    
+                    </div>
+                    <div class="row office-items">
+                        <div class="col-md-12 col-lg-4">                        
+                            <div class="card card-design news-item">
+                                <a href="cambridge-office" >
+                                    <img src="images/cambridge.jpg" alt="Cambridge Office">
+                                </a>                    
+                                <div class="news-wrap">
+                                    <a href="cambridge-office">
+                                        <h4>Cambridge Office</h4>
+                                    </a>
+                                    <p>Unit 1.31,<br>
+                                    St John's Innovation Centre,<br>
+                                    Cowley Road, Milton,<br>
+                                    Cambridge,<br>
+                                    CB4 0WS</p>
+                                    <a href="tel:01223375772" class="phone-number">01223 37 57 72</a>
+                                    <a href="cambridge-office" class="btn btn-sm btn-design">View more</a>           
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 col-lg-4">                        
+                            <div class="card card-design news-item">
+                                <a href="cambridge-office" >
+                                    <img src="images/wymondham.jpg" alt="Wymondham Office">
+                                </a>                    
+                                <div class="news-wrap">
+                                    <a href="wymondham-office">
+                                        <h4>Wymondham Office</h4>
+                                    </a>
+                                    <p>Unit 15,<br>
+                                    Penfold Drive,<br>
+                                    Gateway 11 Business Park,<br>
+                                    Wymondham, Norfolk,<br>
+                                    NR18 0WZ</p>
+                                    <a href="tel:01603704020" class="phone-number">01603 70 40 20</a>
+                                    <a href="wymondham-office" class="btn btn-sm btn-design">View more</a>           
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 col-lg-4">                        
+                            <div class="card card-design news-item">
+                                <a href="cambridge-office" >
+                                    <img src="images/cambridge.jpg" alt="Cambridge Office">
+                                </a>                    
+                                <div class="news-wrap">
+                                    <a href="yarmouth-office">
+                                        <h4>Great Yarmouth Office</h4>
+                                    </a>
+                                    <p>Suite F23,<br>
+                                    Beacon Innovation Centre,<br>
+                                    Beacon Park, Gorleston,<br>
+                                    Great Yarmouth, Norfolk,<br>
+                                    NR31 7RA</p>
+                                    <a href="tel:01493603204" class="phone-number">01493 60 32 04</a>
+                                    <a href="yarmouth-office" class="btn btn-sm btn-design">View more</a>           
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="footing">
+                                <h4 class="section-link"><a href="#">View All <i class="fas fa-arrow-right"></i></a></h4>
+                            </div>                        
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+
+            <!-- About -->
+            <div class="about">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="inner">
+                                <h2>Welcome to Netmatters</h2>
+                                <p><strong>Netmatters is a leading <a href="#">Bespoke Software</a>, <a href="#">IT Support</a>, and <a href="#">Digital Marketing</a> company based in the East of England with offices in <a href="#">Cambridge</a>, <a href="#">Wymondham</a>, and <a href="#">Great Yarmouth</a>.</strong></p>
+                                <p>We aren't tied into contracts with third-party providers, so you know that our recommendations for your business are based purely with one benefit in mind: to help improve your business with the most appropriate solutions.</p>                        
+                                <p>We pride ourselves on being an ethical business and have a unique business offering and cost model that ensures you get the most from our relationship in an upfront manner.</p>
+                                <a href="#" class="btn btn-welcome">
+                                    Read more<i class="fas fa-arrow-right btn-i-last"></i>
+                                </a>
+
+                            </div>                       
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="inner">
+                                <h2>What Our Clients Think</h2>
+                                <p class="five-star">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                </p>    
+                                <p class="client-testimonial">Netmatters stood out from the start. Great guys and very easy to work with. Both the build and digital marketing teams are clearly skilled -they know their stuff! They delivered a website to our (high!) expectations and went over and above to ensure we were satisfied clients - and we are!</p>
+                                <p class="client-name">Eleanor Bishop, Head of Marketing - Ashcroft Partnership LLP</p>
+
+                                <a href="#" class="btn btn-google">
+                                    Google Reviews <i class="fas fa-arrow-right btn-i-last"></i>
+                                </a>
+                                <a href="#" class="btn btn-trust">
+                                    Trustpilot Reviews <i class="fas fa-arrow-right btn-i-last"></i>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Newsletter -->
+            <div class="newsletter">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="inner">
+                                <h2>Email Newsletter Sign-Up</h2>
+                                <div class="input-fields">
+                                    <div class="form-group">
+                                        <label for="name" class="required">Your Name</label>
+                                        <input type="text" id="name" name="name" class="input-field">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email" class="required">Your Email</label>
+                                        <input type="email" id="email" name="email"  class="input-field">
+                                    </div>
+                                </div>
+    
+                                <div class="checkbox-field">
+                                    <div class="element">
+                                        <input type="checkbox" id="opt-in" name="opt-in" class="opt-in">
+                                        <label for="opt-in"></label>
+                                    </div>
+                                    <div class="description">
+                                        <label for="opt-in">Please tick this box if you wish to receive marketing information from us. Please see our Privacy Policy for more information on how we keep your data safe.</label>
+                                    </div>
+                                </div>
+
+                                <a href="#" class="btn btn-subscribe btn-regular">
+                                    Subscribe
+                                </a>
+
+                            </div>                       
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Footer -->
+            <div class="footer">
+                <div class="container">
+                    <div class="footer-menus">
+                        <div class="service-list">
+                            <div class="menu">
+                                <h4>About Netmatters</h4> 
+                                <ul>
+                                    <li><a href="#">News</a></li>
+                                    <li><a href="#">Our Careers</a></li>
+                                    <li><a href="#">Our Team</a></li>
+                                    <li><a href="#">Privacy Policy</a></li>
+                                    <li><a href="#">Cookie Policy</a></li>
+                                    <li><a href="#">Terms & Conditions</a></li>
+                                    <li><a href="#">Environmental Policy</a></li>
+                                    <li><a href="#">Contact Us</a></li>
+                                </ul>
+                            </div>
+                            <div class="menu">
+                                <h4>Our Services</h4> 
+                                <ul>
+                                    <li><a href="#">Bespoke Software</a></li>
+                                    <li><a href="#">IT Support</a></li>
+                                    <li><a href="#">Digital Marketing</a></li>
+                                    <li><a href="#">Telecoms Services</a></li>
+                                    <li><a href="#">Web Design</a></li>
+                                    <li><a href="#">Cyber Security</a></li>
+                                    <li><a href="#">Developer Training</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="office-list">
+                            <div class="menu">
+                                <h4>Cambridge office</h4> 
+                                <p> 
+                                    Unit 1.31, <br>
+                                    St John's Innovation Centre,<br>
+                                    Cowley Road, Milton,<br>
+                                    Cambridge,<br>
+                                    CB4 0WS
+                                </p>
+                                <p>     
+                                    Tel: <a href="tel:01223375772">01223 37 57 72</a>
+                                </p>
+                            </div>
+                            <div class="menu">
+                                <h4>Wymondham Office</h4> 
+                                <p> 
+                                    Unit 15,<br>
+                                    Penfold Drive,<br>
+                                    Gateway 11 Business Park,<br>
+                                    Wymondham, Norfolk,<br>
+                                    NR18 0WZ
+                                </p>
+                                <p>     
+                                    Tel: <a href="tel:01603704020">01603 70 40 20</a>
+                                </p>
+                            </div>
+                            <div class="menu">
+                                <h4>Great Yarmouth Office</h4> 
+                                <p> 
+                                    Suite F23,<br>
+                                    Beacon Innovation Centre,<br>
+                                    Beacon Park, Gorleston,<br>
+                                    Great Yarmouth, Norfolk,<br>
+                                    NR31 7RA
+                                </p>
+                                <p>     
+                                    Tel: <a href="tel:01493603204">01493 60 32 04</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>  
+                    <div class="bottom-bar">
+                        <div class="copyright">
+                            <p>
+                                &copy; Copyright Netmatters 2023. <br> All rights reserved.                             
+                            </p>
+                            <span> - </span>
+                            <a href="#">Sitemap</a>
+                        </div>
+                        <div class="social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>             
+                </div>
+            </div>
+
+<?php
+@include 'includes/footer.php';
+?>
