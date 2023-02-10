910e0e0d6331c4a622e3a0971731e8190bb17e6b
chiefofstack <chiefofstack@gmail.com>
2023-02-10 13:20:05 +0000

    apply session variables to the form

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
 <?php
 $currentPage = "Contact";
+include 'includes/session.php';
 include 'includes/header.php';
 ?>
             <!-- Offices -->
@@ -112,25 +113,43 @@ include 'includes/header.php';
                         </div>
                         <div class="col-sm-12 col-xl-8 order-xl-1">
                             <form action="process.php" method="post">
+                                <input type="hidden" name="enquire" value="1">
                                 <div class="contact-form">
+                                    <a id="contact-form"></a>
                                     <div class="row">    
                                         <div class="col-md-12">
+                                            <?php if($_SESSION['success'] == 0){ ?>
                                             <div class="alert alert-danger" >
-                                                There were errors in the form.
+                                                There were errors found in the form.
                                             </div>
+                                            <?php } else if($_SESSION['success'] == -1) { ?>
+                                            <div class="alert alert-warning" >
+                                                Could not connect to the database
+                                            </div>
+                                            <?php } else if($_SESSION['success'] == -2) { ?>
+                                            <div class="alert alert-warning" >
+                                                Could not save to the database
+                                            </div>
+                                            <?php } else { ?>
+                                            <div class="alert alert-success" >
+                                                Your message has been sent!
+                                            </div>
+                                            <?php } ?>
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="col-md-12 col-lg-6">
                                             <div class="form-group">
                                                 <label for="name" class="required">Your Name</label>
-                                                <input type="text" id="name" name="name" class="input-field">
+                                                <input type="text" id="name" name="name" class="input-field <?php if($form->getError('name')) echo "error"; ?>" value="<?php echo $form->getValue('name'); ?>">
+                                                <span class="error-message"><?php echo $form->getError('name'); ?></span>
                                             </div>
                                         </div>
                                         <div class="col-md-12 col-lg-6">
                                             <div class="form-group">
                                                 <label for="company" >Company Name</label>
-                                                <input type="text" id="company" name="company"  class="input-field">
+                                                <input type="text" id="company" name="company"  class="input-field <?php if($form->getError('company')) echo "error"; ?>" value="<?php echo $form->getValue('company'); ?>">
+                                                <span class="error-message"><?php echo $form->getError('company'); ?></span>
                                             </div>
                                         </div>
                                     </div>
@@ -138,13 +157,15 @@ include 'includes/header.php';
                                         <div class="col-md-12 col-lg-6">
                                             <div class="form-group">
                                                 <label for="email" class="required">Your Email</label>
-                                                <input type="text" id="email" name="email" class="input-field">
+                                                <input type="text" id="email" name="email" class="input-field <?php if($form->getError('email')) echo "error"; ?>" value="<?php echo $form->getValue('email'); ?>">
+                                                <span class="error-message"><?php echo $form->getError('email'); ?></span>
                                             </div>
                                         </div>
                                         <div class="col-md-12 col-lg-6">
                                             <div class="form-group">
                                                 <label for="telephone" class="required">Your Telephone Number</label>
-                                                <input type="text" id="telephone" name="telephone"  class="input-field">
+                                                <input type="text" id="telephone" name="telephone"  class="input-field <?php if($form->getError('telephone')) echo "error"; ?>" value="<?php echo $form->getValue('telephone'); ?>">
+                                                <span class="error-message"><?php echo $form->getError('telephone'); ?></span>
                                             </div>
                                         </div>
                                     </div>
@@ -152,7 +173,8 @@ include 'includes/header.php';
                                         <div class="col-12">
                                             <div class="form-group">
                                                 <label for="subject" class="required">Subject</label>
-                                                <input type="text" id="subject" name="subject" class="input-field">
+                                                <input type="text" id="subject" name="subject" class="input-field <?php if($form->getError('subject')) echo "error"; ?>" value="<?php echo $form->getValue('subject'); ?>">
+                                                <span class="error-message"><?php echo $form->getError('subject'); ?></span>
                                             </div>
                                         </div>
                                     </div>
@@ -160,7 +182,8 @@ include 'includes/header.php';
                                         <div class="col-12">
                                             <div class="form-group">
                                                 <label for="message" class="required">Message</label>
-                                                <textarea id="message" name="message"  id="message" rows="20" class="input-field message"></textarea>
+                                                <textarea id="message" name="message"  rows="20" class="input-field message <?php if($form->getError('message')) echo "error"; ?>"><?php echo $form->getValue('message'); ?></textarea>
+                                                <span class="error-message"><?php echo $form->getError('message'); ?></span>
                                             </div>
                                         </div>
                                     </div>
@@ -179,9 +202,9 @@ include 'includes/header.php';
                                         </div>
                                     </div>
 
-                                    <a href="#" class="btn btn-design btn-regular">
+                                    <button type="submit" class="btn btn-design btn-regular">
                                         Send  Enquiry
-                                    </a>
+                                    </button>
                                 </div>
                             </form>                
                         </div>
