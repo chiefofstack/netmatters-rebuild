624df3f9ef70cddd963fc14cd3b53c408ec525dc
chiefofstack <chiefofstack@gmail.com>
2023-02-10 01:08:41 +0000

    new newsletter.php for newsletter section

---
+++ includes/newsletter.php
@@ -0,0 +1,37 @@
+            <!-- Newsletter -->
+            <div class="newsletter">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="inner">
+                                <h2>Email Newsletter Sign-Up</h2>
+                                <div class="input-fields">
+                                    <div class="form-group">
+                                        <label for="name" class="required">Your Name</label>
+                                        <input type="text" id="name" name="name" class="input-field">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email" class="required">Your Email</label>
+                                        <input type="email" id="email" name="email"  class="input-field">
+                                    </div>
+                                </div>
+    
+                                <div class="checkbox-field">
+                                    <div class="element">
+                                        <input type="checkbox" id="opt-in" name="opt-in" class="opt-in">
+                                        <label for="opt-in"></label>
+                                    </div>
+                                    <div class="description">
+                                        <label for="opt-in">Please tick this box if you wish to receive marketing information from us. Please see our <a href="#">Privacy Policy</a> for more information on how we keep your data safe.</label>
+                                    </div>
+                                </div>
+
+                                <a href="#" class="btn btn-subscribe btn-regular">
+                                    Subscribe
+                                </a>
+
+                            </div>                       
+                        </div>
+                    </div>
+                </div>
+            </div>
